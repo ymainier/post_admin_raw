56cd513ac1fd3cf7654dfdf27341aa8d621c9c33
Yann Mainier <yann.mainier@gmail.com>
2011-08-17 13:27:27 +0200

    Les post sont gérable (CRUD) dan sle backoffice, sinon seulement accessible en index/show depuis /

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 PostAdmin::Application.routes.draw do
-  resources :posts
+  resources :posts, :only => [:index, :show]
+
+  namespace "admin" do
+    resources :posts
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
