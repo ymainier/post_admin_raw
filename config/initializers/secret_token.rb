ffcd41172d04ebc8849d341b05258b6dc8ea0b29
Yann Mainier <yann.mainier@gmail.com>
2011-08-17 12:32:58 +0200

    Initialisation avec des posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostAdmin::Application.config.secret_token = '2e8067015b24851abc30db2acb94eff55150be0a652af7ff5f8a490bd8a7c7400023c960f91dfe8c573b6085777f7b3ce53cd18b40a785387a6c0e0527beed1e'
